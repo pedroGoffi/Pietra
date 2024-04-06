ab7c32806d5c936b377abb04b43c4fdaf5e20b11
pedroGoffi <pedrohgdepauloc@gmail.com>
2024-04-05 23:19:57 -0300

    @pedroGoffi[Added Release and Debug]
    Release file for normal usage while Debug is been used to improve the
    language itself

---
+++ release/CMakeFiles/pi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pi.dir/main.cpp.o"
+  "CMakeFiles/pi.dir/main.cpp.o.d"
+  "pi"
+  "pi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- release/CMakeFiles/pi.dir/flags.make
+++ release/CMakeFiles/pi.dir/flags.make
@@ -6,5 +6,5 @@ CXX_DEFINES =
 
 CXX_INCLUDES = 
 
-CXX_FLAGS = -Wall -Wextra -std=gnu++20
+CXX_FLAGS = -Wall -Wextra -O3 -std=gnu++20
 
